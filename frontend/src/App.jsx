3e0afabdf3cea835244d0dcec816e1d089b1ccb4
Max chilo <maxchiloapaza@gmail.com>
2026-06-25 15:45:07 -0500

    error corregido fondo azul

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,23 +2,21 @@ import { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './routes/AppRouter';
 import Navbar from './components/layout/Navbar';
-import CarritoDrawer from './components/carrito/CarritoDrawer';
 import Footer from './components/layout/Footer';
+import CarritoDrawer from './components/carrito/CarritoDrawer';
 
 function App() {
   const [carritoAbierto, setCarritoAbierto] = useState(false);
 
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-gray-900">
-        <Navbar onAbrirCarrito={() => setCarritoAbierto(true)} />
-        <AppRouter />
-        <Footer />
-        <CarritoDrawer
-          abierto={carritoAbierto}
-          onCerrar={() => setCarritoAbierto(false)}
-        />
-      </div>
+      <Navbar onAbrirCarrito={() => setCarritoAbierto(true)} />
+      <AppRouter />
+      <Footer />
+      <CarritoDrawer
+        abierto={carritoAbierto}
+        onCerrar={() => setCarritoAbierto(false)}
+      />
     </BrowserRouter>
   );
 }
